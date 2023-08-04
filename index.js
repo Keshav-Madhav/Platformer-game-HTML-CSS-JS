c56b631aada4bc0cd109dc4b4e8c4389edeb1241
Keshav <keshav2552003@gmail.com>
2023-08-04 15:17:04 +0530

    Basic setup, canvas, delta time, movement

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const canvas = document.getElementById('canvas');
-const c= canvas.getContext('2d');
+let board;
+let boardWidth = window.innerWidth;
+let boardHeight = window.innerHeight;
+let context;
 
+let y = 100;
+let velocity = 100;
+let lastFrameTime = null;
+
+window.onload = function() {
+    board = document.getElementById('board');
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext('2d');
+
+    context.fillStyle = 'red';
+    context.fillRect(100, 100, 100, 100);
+
+    requestAnimationFrame(update);
+}
+
+function update(timestamp) {
+    let deltaTime = getDeltaTime(timestamp);
+    y += velocity * deltaTime;
+
+    context.clearRect(0, 0, board.width, board.height);
+
+    context.fillStyle = 'red';
+    context.fillRect(100, y, 100, 100);
+
+    requestAnimationFrame(update);
+}
+
+
+
+window.addEventListener("resize", function() {
+    board.width = window.innerWidth;
+    board.height = window.innerHeight;
+});
+
+function getDeltaTime(timestamp) {
+    if (lastFrameTime === null) {
+        lastFrameTime = timestamp;
+    }
+    let deltaTime = (timestamp - lastFrameTime) / 1000; // convert to seconds
+    lastFrameTime = timestamp;
+
+    return deltaTime;
+}
